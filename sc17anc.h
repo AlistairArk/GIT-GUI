aa15daf3c852573916db7a72d9d13c47d77f64e8
Andrea <sc17anc@leeds.ac.uk>
2018-12-10 09:33:56 +0000

    add header

---
+++ sc17anc.h
@@ -0,0 +1,17 @@
+#include <QLineEdit>
+#include <QString>
+#include <QGridLayout>
+
+class SearchTab : public QWidget{
+  Q_OBJECT
+
+  public:
+  SearchTab();
+
+  private:
+    QGridLayout *Search;
+    QLineEdit *keyword;
+
+  private slots:
+    void SearchClicked();
+};
