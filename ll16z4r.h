d72ee5282a7999e1e38f612cb5e74099ab162de0
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2018-12-11 20:10:50 +0000

    Comments added

--- ll16z4r.h
+++ ll16z4r.h
@@ -11,6 +11,8 @@ class QLabel;
 class QWidget;
 
 namespace stats{
+
+// store the totals by each author
 struct CommitData {
 	size_t insertions;
 	size_t deletions;
@@ -38,11 +40,13 @@ private: // widgets needed
 	void            repoInit();
 	void            loadData();
 	bool            m_skip_list;    // avoid updates to the list
+    // std::map to store the totals by author
 	std::map<std::string, CommitData>     m_commits;
 	size_t          total_insertions;
 	size_t          total_deletions;
 	size_t          total_files_changed;
 	size_t          total_commits;
+    // number of columns of the listing and variables to store
 	int COL_GITLIST_COUNT;
 	int COL_GITLIST_ITEM;
 	int COL_GITLIST_AUTHOR;
