ae0b220ea1f952a2e1bf7f0617e72ca1388b96cd
Alistair J.H. Donnell <sc17jhd@leeds.ac.uk>
2018-12-11 11:13:59 +0000

    header updates for changes in sc17jhd.cc

--- sc17jhd.h
+++ sc17jhd.h
@@ -1,17 +1,19 @@
 #include <QWidget>
 #include <QListWidget>
 
+
 class QPushButton;
 
 class BranchHandler : public QWidget{
     Q_OBJECT
 
-  public:
-    BranchHandler();
+    public:
+        BranchHandler();
+        int branchIndex=0;
 
-  private slots:
-    void on_pushButton_clicked();
-  private:
-    QListWidget *listWidget;
-    int choice=0; // Set master branch as default
+    private slots:
+        void on_pushButton_clicked();
+        void populateList();
+    private:
+        QListWidget *listWidget;
 };
