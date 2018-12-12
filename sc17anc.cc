307747ed6a92ae818f41816da5b34a21d85af5c3
Andrea <sc17anc@leeds.ac.uk>
2018-12-12 22:20:14 +0000

    error if no repo selected

--- sc17anc.cc
+++ sc17anc.cc
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QHeaderView>
 #include <QScrollBar>
+#include <QMessageBox>
 
 namespace search{
 
@@ -24,45 +25,58 @@ SearchTab::SearchTab(){
 }
 	void SearchTab::SearchClicked()
 	{
-		if(Commits == NULL)
+		try
 		{
-			Commits = new QTableWidget();
-			Search->addWidget(Commits);
-		}
-		else
-		{
-			for(int i = Commits->rowCount(); i >= 0; i--)
+			GITPP::REPO r(myDirStr);
+
+			if(Commits == NULL)
 			{
-				Commits->removeRow(i);
+				Commits = new QTableWidget();
+				Search->addWidget(Commits, 2, 0, 1, 3);
 			}
-		}
-		Commits->setColumnCount(3);
-		Header<<"Date"<<"Author"<<"Message";
-		Commits->setHorizontalHeaderLabels(Header);
-		Commits->verticalScrollBar()->setDisabled(false);
-		Commits->verticalHeader()->setVisible(false);
-
-		GITPP::REPO r(myDirStr);
-		std::string data = keyword->text().toStdString();
-
-		int x = 0;
-		for(auto i:r.commits())
-		{
-			if(i.message().find(data) != std::string::npos)
+			else
 			{
-				Commits->horizontalHeader()->setStretchLastSection(true);
-				QString t = QString::fromStdString(i.time());
-				QString author = QString::fromStdString(i.signature().name());
-				QString message = QString::fromStdString(i.message());
-				Commits->insertRow(x);
-				Commits->setItem(x, 0, new QTableWidgetItem(t));
-				Commits->setItem(x, 1, new QTableWidgetItem(author));
-				Commits->setItem(x, 2, new QTableWidgetItem(message));
-
-				x++;
+				for(int i = Commits->rowCount(); i >= 0; i--)
+				{
+					Commits->removeRow(i);
+				}
 			}
+			Commits->setColumnCount(3);
+			Header<<"Date"<<"Author"<<"Message";
+			Commits->setHorizontalHeaderLabels(Header);
+			Commits->verticalScrollBar()->setEnabled(true);
+			Commits->verticalHeader()->setVisible(false);
+			Commits->setEditTriggers(QAbstractItemView::NoEditTriggers);
+			Commits->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+			Commits->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+
+			std::string data = keyword->text().toStdString();
+
+			int x = 0;
+			for(auto i:r.commits())
+			{
+				if(i.message().find(data) != std::string::npos)
+				{
+					QString t = QString::fromStdString(i.time());
+					QString author = QString::fromStdString(i.signature().name());
+					QString message = QString::fromStdString(i.message());
+					Commits->insertRow(x);
+					Commits->setItem(x, 0, new QTableWidgetItem(t));
+					Commits->setItem(x, 1, new QTableWidgetItem(author));
+					Commits->setItem(x, 2, new QTableWidgetItem(message));
+
+					x++;
+				}
+			}
+			setLayout(Search);
 		}
-		setLayout(Search);
+		catch(GITPP::EXCEPTION_CANT_FIND const&)
+		{
+			QMessageBox::information(
+				this,
+				tr("Warning!"),
+				tr("No git repository selected"));
+		}
 	}
 
 	INSTALL_TAB(SearchTab, "Search");
